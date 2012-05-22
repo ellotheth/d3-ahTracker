784f8ec02201e624571956bf9c3f55406f330cb0
ellotheth <ellotheth@gmail.com>
2012-05-21 19:07:24 -0600

    Add the 're' package to the build

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from cx_Freeze import setup, Executable
 
 # Run python setup.py build to create the exe
 
+# might need this too? "includes":["tkinter._fix"]
+build_exe_options = {"packages":["re"]}
+
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
@@ -10,5 +13,6 @@ if sys.platform == "win32":
 setup(name = "ahCalculator",
       version = "0.02",
       description = "Diablo 3 auction house calculator",
+      options = {"build_exe": build_exe_options},
       executables = [Executable("ahCalculator.pyw", base=base)])
 
